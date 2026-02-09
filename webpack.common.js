7bf31b5e5b0525125ddb142172f33c29ab6307fc
Marwan Mohamed Amin <marwan44amin@gmail.com>
2026-02-09 17:01:38 +0200

    webpack.common.js update

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,17 @@
 import path from "node:path";
+import { fileURLToPath } from 'node:url';
 import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         clean: true,
     },
-    
+
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
